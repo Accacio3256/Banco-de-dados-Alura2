c3d34d5d41fdbff4b5ed3f477a910ecfafa514aa
Rafael-Saldanha <rafael.saldanha@aluno.senai.br>
2025-07-03 11:28:47 -0300

    Incompleto (por enquanto)

---
+++ Script-Alura2.sql
@@ -0,0 +1,57 @@
+create table curso (
+	curso_id serial,
+	nome_curso varchar (60),
+	descricao text,
+	duracao int,
+	ultima_atualizacao timestamp	
+);
+
+create table categoria(
+	categoria_id serial,
+	nome_categoria varchar (30),
+	ultima_atualizacao timestamp
+);
+
+create table curso_categoria(
+	curso_id int,
+	categoria_id int,
+	ultima_atualizacao timestamp
+);
+
+create table planos(
+	planos_id serial,
+	nome_plano char(6),
+	vencimento time, 
+	valor (7,2),
+	ultima_atualizacao timestamp
+);
+
+create table pagamento(
+	pagamento_id serial,
+	planos_id int,
+	dia_pagamento time, 
+	usuario_id int,
+	ultima_atualizacao timestamp
+);
+
+create table usuario (
+	usuario_id serial,
+	nome_usuario char (30),
+	sobrenome char (30),
+	email varchar (50),
+	senha varchar (30),
+	data_de_criacao date,
+	ultima_atualizacao timestamp
+);
+
+create table video_aula (
+	video_aula_id serial,
+	curso_id int,
+	nome_video_aula varchar(30)
+);
+
+create table login (
+	login_id serial,
+	cpf char(11),
+	
+);
